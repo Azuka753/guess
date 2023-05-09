2de278ee16e15c03e34be6611448d4e0e29ad916
Azuka753 <lightupthedark@qq.com>
2023-05-09 18:42:00 +0800

    tips changed

--- guess.go
+++ guess.go
@@ -10,14 +10,13 @@ import (
 )
 
 func main(){
-	fmt.Println("猜数字")
+	fmt.Println("猜100内的数字")
 	number := rand.Intn(100)
 	reader := bufio.NewReader(os.Stdin)
 	Loop:
 	for {
 		s, _ := reader.ReadString('\n')
 		i, _ := strconv.Atoi(strings.TrimSpace(s))
-		fmt.Println("你输入的数字是：",  i)
 		switch{
 		case i > number:
 			fmt.Println("猜大了！")
