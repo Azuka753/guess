ca0706c4b74f7905a37eed297ec1ceaf8d792edf
Azuka753 <lightupthedark@qq.com>
2023-05-09 18:44:30 +0800

    merge

--- guess.go
+++ guess.go
@@ -13,17 +13,19 @@ func main(){
 	fmt.Println("猜100内的数字")
 	number := rand.Intn(100)
 	reader := bufio.NewReader(os.Stdin)
+	count := 0
 	Loop:
 	for {
 		s, _ := reader.ReadString('\n')
 		i, _ := strconv.Atoi(strings.TrimSpace(s))
+		count++
 		switch{
 		case i > number:
 			fmt.Println("猜大了！")
 		case i < number:
 			fmt.Println("猜小了！")
 		case i == number:
-			fmt.Println("猜对了！")
+			fmt.Println("猜对了！你一共猜了", count, "次")
 			break Loop
 		}
 	}
